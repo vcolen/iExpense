b6e557e4cbc6eac5738615a16cb36a4f87816cd1
VIctor <colen.victor@icloud.com>
2021-12-10 16:15:02 -0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Victor Colen on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
